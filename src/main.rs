5ee7add2b01c6a8cf7737a26c9ec99618a4eb212
EvilMadSquirrel <minichev.s.l@gmail.com>
2022-10-31 18:29:37 +0300

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
 fn main() {
-    println!("Hello, world!");
+    let days = [
+        "First",
+        "Second",
+        "Third",
+        "Fourth",
+        "Fifth",
+        "Sixth",
+        "Seventh",
+        "Eighth",
+        "Ninth",
+        "Tenth",
+        "Eleventh",
+        "Twelfth"
+    ];
+    let brought = [
+        "A song and a Christmas tree",
+        "Two candy canes",
+        "Three boughs of holly",
+        "Four colored lights",
+        "A shining star",
+        "Little silver bells",
+        "Candles a-glowing",
+        "Gold and silver tinsel",
+        "A guardian angel",
+        "Some mistletoe",
+        "Gifts for one and all",
+        "All their good wishes"
+    ];
+
+    let mut count = 1;
+
+    for day in days {
+        println!("On the {} day of Christmas", day);
+        println!("My good friends brought to me");
+        for i in (0..count).rev() {
+            let gift = brought[i];
+            println!("{}", gift);
+        }
+        count += 1;
+        println!("\n");
+    }
 }
